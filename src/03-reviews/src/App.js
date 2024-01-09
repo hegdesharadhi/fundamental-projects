3d8519ad296a8ceca613216f688c358a4898f98a
Sharadhi Hegde <hegdesharadhi@gmail.com>
2024-01-09 23:00:06 +0530

    03-reviews Mini Project

--- src/03-reviews/src/App.js
+++ src/03-reviews/src/App.js
@@ -1,4 +1,72 @@
+import { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>
+  const [index, setIndex] = useState(0)
+  const { name, image, job, text } = people[index]
+
+  /* const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }*/
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = (index + 1) % people.length
+      return newIndex
+      //  return checkNumber(newIndex)
+    })
+  }
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = (index - 1 + people.length) % people.length
+      return newIndex
+
+      //return checkNumber(newIndex)
+    })
+  }
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length)
+    if (randomNumber === index) {
+      randomNumber = index + 1
+    }
+    const newIndex = randomNumber % people.length
+    setIndex(newIndex)
+    // setIndex(checkNumber(randomNumber))
+  }
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-con">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          surprise me
+        </button>
+      </article>
+    </main>
+  )
 }
 export default App
